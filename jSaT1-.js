4f0dd9cb6dd3d528b04c8426d233cac1a7e2d617
HenthonyGuilherme <henthonyguilhermeferreira.2021@gmail.com>
2021-09-26 19:56:23 -0300

    T_T

---
+++ jSaT1-.js
@@ -0,0 +1,23 @@
+//!º) você é maior de 18 anos?
+var maiorDe18; // true false
+
+if (maiorDe18 == true){
+  console.log("Que legal! Você já pode ter a carteira de habilitação.");
+}
+else{
+  console.log("Poxa, que pena! Você ainda não pode ter a carteira de habilitação.");
+  
+}
+
+// 2º) você já terminou o ensino médio?
+var ensinoMedio; // true false
+
+if (ensinoMedio == true){
+  console.log("Poxa, que legal!.");
+  
+}
+else{
+  console.log("Falta pouco! Logo você termina.");
+  
+  
+}
---
+++ jSaT3-.js
@@ -0,0 +1,29 @@
+// 1º) Exibir no console o resultado das seguintes validações, dado os valores x = 5 e y = 7.
+
+var x = 5;
+var y = 7;
+
+if (x < y){
+  console.log('Olá mundo!');
+}
+else if (x > y){
+  console.log('Mundo, olá!');
+}
+else{
+  console.log('Adeus!');
+}
+
+
+// 2º) Exibir no console o resultado das seguintes validações,vamos dizer o filme que está passando no cinema de acordo com a fruta, dado o valor de fruta = laranja.
+
+var fruta = 'laranja';
+
+if (fruta == 'laranja'){
+  console.log('O filme é Laranja Mecânica.');
+}
+else if (fruta == 'maçã'){
+  console.log('O filme é Branca de neve.');
+}
+else {
+  console.log('O cinema tá fechado.');
+}
